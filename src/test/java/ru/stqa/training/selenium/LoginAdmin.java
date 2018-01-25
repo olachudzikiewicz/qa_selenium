f79c795d6d572d7ed4843cb20a928af5cecd7e9b
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2018-01-25 20:58:57 +0100

    Test logowania panel admin

--- src/test/java/ru/stqa/training/selenium/LoginAdmin.java
+++ src/test/java/ru/stqa/training/selenium/LoginAdmin.java
@@ -5,16 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
-public class MyFirsTest {
+public class LoginAdmin {
 
   private WebDriver driver;
   private WebDriverWait wait;
@@ -22,18 +19,17 @@ public class MyFirsTest {
   @Before
   public void start() {
     driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //selenium będzie czekać max 10 sek aż się
-    // jakiś element pojawi
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
-  public void myFirstTest() {
+  public void testLoginAdmin() {
 
-    driver.navigate().to("http://www.google.com"); //driver.get("http://www.google.com");
-    driver.findElement(By.name("q")).sendKeys("webdriver");
-    driver.findElement(By.name("btnK")).click();
-  //  wait.until(titleIs("webdriver - Поиск в Google"));
+    driver.navigate().to("http://localhost/litecart/admin/login.php");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
   }
 
   @After
@@ -41,5 +37,4 @@ public class MyFirsTest {
     driver.quit();
     driver = null;
   }
-
 }
