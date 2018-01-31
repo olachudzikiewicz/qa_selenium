06b48411a90d16354087dd8bcf3c520b08440644
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2018-01-31 21:09:56 +0100

    Test MenuClick

--- src/test/java/ru/stqa/training/selenium/MenuClick.java
+++ src/test/java/ru/stqa/training/selenium/MenuClick.java
@@ -1,5 +1,6 @@
 package ru.stqa.training.selenium;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -30,14 +31,16 @@ public class MenuClick {
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("login")).click();
 
+
     //Menu:
     driver.findElement(By.xpath("//span[contains(text(),'Appearence')]")).click();
     driver.findElement(By.xpath("//span[contains(text(),'Template')]")).click();
+    Assert.assertEquals(driver.findElement(By.xpath("//span[contains(text(),'Template')]")).getText(),
+            driver.findElement(By.xpath("//h1[contains(@style,'margin-top')]")).getText());
     driver.findElement(By.xpath("//span[contains(text(),'Logotype')]")).click();
-    String h2 = driver.findElement(By.xpath("//span[contains(text(),'Template')]")).getText();
-    String h1 = driver.findElement(By.xpath("//h1[contains(text(),'margin-top'")).getText();
 
-   /* driver.findElement(By.xpath("//span[contains(text(),'Catalog')]")).click();
+
+    driver.findElement(By.xpath("//span[contains(text(),'Catalog')]")).click();
     driver.findElement(By.xpath("//li[@id='doc-catalog']//span[contains(text(),'Catalog')]")).click(); //!!!
     driver.findElement(By.xpath("//span[contains(text(),'Product Groups')]")).click();
     driver.findElement(By.xpath("//span[contains(text(),'Option Groups')]")).click();
@@ -79,15 +82,37 @@ public class MenuClick {
     driver.findElement(By.xpath("//a[contains(@href,'pages&doc=pages')]")).click();
 
     driver.findElement(By.xpath("//span[contains(text(),'Reports')]")).click();
+    driver.findElement(By.xpath("//span[contains(text(),'Monthly Sales')]")).click();
+    driver.findElement(By.xpath("//span[contains(text(),'Most Sold Products')]")).click();
+    driver.findElement(By.xpath("//span[contains(text(),'Most Shopping Customers')]")).click();
 
-*/
+     driver.findElement(By.xpath("//span[contains(text(),'Settings')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Store Info')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Defaults')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'General')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Listings')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Images')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Checkout')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Advanced')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Security')]")).click();
 
+    driver.findElement(By.xpath("//span[contains(text(),'Slides')]")).click();
 
+     driver.findElement(By.xpath("//a[contains(@href,'tax_classes')]")).click();
+     driver.findElement(By.xpath("//li[@id='doc-tax_classes']//span[contains(text(),'Tax Classes')]")).click();
+     driver.findElement(By.xpath("//li[@id='doc-tax_rates']//span[contains(text(),'Tax Rates')]")).click();
 
+     driver.findElement(By.xpath("//span[contains(text(),'Translations')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Search Translations')]")).click();
+     driver.findElement(By.xpath("//span[contains(text(),'Scan Files')]")).click();
+     driver.findElement(By.xpath("//li[@id='doc-csv']//span[contains(text(),'CSV Import/Export')]")).click();
 
+     driver.findElement(By.xpath("//span[contains(text(),'Users')]")).click();
 
-
-
+     driver.findElement(By.xpath("//span[contains(text(),'vQmods')]")).click();
+    Assert.assertEquals(driver.findElement(By.xpath("//span[contains(text(),'vQmods')]")).getText(),
+            driver.findElement(By.xpath("//h1[contains(@style,'margin-top')]")).getText());
+     driver.findElement(By.xpath("//li[@id='doc-vqmods']//span[contains(text(),'vQmods')]")).click();
 
   }
 }
